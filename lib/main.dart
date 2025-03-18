0c9bfe61a04b6cf61f1f927d087e0ae5350c5fd2
Sumit Dhungana <sumitdhungana14@8e-39-71-07-0c-1e.wlan.lsu.edu>
2025-03-18 13:18:03 -0500

    Daily remainders notification on iOS and Android.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:project_proud_me/constant.dart';
 import 'package:project_proud_me/introduction/introduction.dart' show Introduction;
 import 'package:project_proud_me/language.dart';
+import 'package:project_proud_me/service/notification_service.dart';
 
-void main() {
+final GlobalKey<NavigatorState> dialogKey = GlobalKey<NavigatorState>();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initNotifications();
   runApp(const MyApp());
+}
+
+Future<void> initNotifications() async {
+  NotificationService notificationService = NotificationService();
+  await notificationService.init();
+  if (Platform.isIOS) {
+    await notificationService.requestIOSPermissions();
+  } else if (Platform.isAndroid) {
+    await notificationService.requestAndroidPermission();
+  }
+  await notificationService.scheduleNotification(
+    1, 
+    "Daily Remainder", 
+    "Set daily goals and track behaviors on ProudMe App.", 
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day), 
+    const TimeOfDay(hour: 8, minute: 00), 
+    "", 
+    "daily", 
+    1);
 }
 
 class MyApp extends StatelessWidget {
@@ -13,6 +38,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: dialogKey,
       title: projectTitle,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
